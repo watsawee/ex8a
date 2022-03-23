6c7e3fdf9ac93f44edd28b93460989676892d2e9
hatsawat <hatsawat.yamthung@gmail.com>
2022-03-23 15:21:36 +0700

    adding word count feature

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,15 @@ function file_readline(){
   const contents = fs.readFileSync('public\\input.txt', 'utf-8');
   contents.split(/\r?\n/).forEach(line => {
   console.log(`Line from file: ${line}`);
+  if(line == fav_team)cnt++;
   });
+  return cnt;
   }
 
 router.get('/wordcnt/:fav_team', function (req, res) {
   var fav_team = req.params.fav_team;
-  file_readline();
+  var cnt = file_readline(fav_team);
+  console.log('word count ->'+ cnt);  
   res.send("word count API - "+fav_team);	
 });
 
