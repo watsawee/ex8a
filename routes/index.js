040847851d0ce99e43b87868c8f05b74a23a23dd
Chritmas Jamsri <krisamas1996@gmail.com>
2022-03-23 15:03:12 +0700

    adding readline fn

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function file_readline(){
+const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+contents.split(/\r?\n/).forEach(line => {
+console.log(`Line from file: ${line}`);
+});
+}
+
 router.get('/wordcnt/:fav_team', function (req, res) {
   var fav_team = req.params.fav_team;
+  file_readline();
   res.send("word count API - "+fav_team);	
 });
 
