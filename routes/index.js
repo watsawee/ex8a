126cc265f77da3f26726aac48bbd0bce62706243
Akarin Srianan <Sodza23@gmail.com>
2023-03-16 11:53:44 +0700

    add parameler receiving code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 var fs = require('fs');
-function file_readline(){  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+function file_readline(fav_team)
+{ let cnt =0;
+    const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+
   contents.split(/\r?\n/).forEach(line =>  {
     console.log(`Line from file: ${line}`);
   });
@@ -12,7 +15,7 @@ function file_readline(){  const contents = fs.readFileSync('public\\input.txt',
 router.get('/wordcnt/:fav_team', function(req, res) {
   let fav_team =req.params.fav_team
   let cnt =file_readline(fav_team)
-  res.send("word count API - " +fav_team);
+  res.send("word count API - " +fav_team+"("+cnt+")");
 });
 
 module.exports = router;
