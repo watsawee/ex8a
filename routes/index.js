891eea1834f57e0eeab41e1c3415dbdd837d5b38
Akarin Srianan <Sodza23@gmail.com>
2023-03-16 11:45:41 +0700

    add a readlinr function

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var fs = require('fs');
+function file_readline(){  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
 
 router.get('/wordcnt/:fav_team', function(req, res) {
+  let fav_team =req.params.fav_team
+  let cnt =file_readline(fav_team)
   res.send("word count API - " +fav_team);
 });
 
