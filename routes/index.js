a8f4af1af641e36af478d4fe366279118e147775
kittikorn <kittkorn.not@gmail.com>
2023-03-16 11:21:02 +0700

    adding readline function

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var fs = require('fs')
+
+function file_readline(){  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
+
 router.get('/wordcnt', function(req, res) {
   let fav_team = req.params.fav_team;
+  file_readline();
   res.send("word count API - " + fav_team);
 });
 
 module.exports = router;
+
