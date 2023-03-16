723d7251e90bcf7282cbc02b9fe1e3348d87e8d0
JackJack <2110511101032@live4.utcc.ac.th>
2023-03-16 11:21:26 +0700

    add a readline function

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var fs = require('fs');
+function file_readline()
+{  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
+
 router.get('/wordcnt/:fav_team', function(req, res) {
   let fav_team = req.params.fav_team;
   res.send("word count API");
 });
 
 module.exports = router;
+
