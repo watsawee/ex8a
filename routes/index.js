427181c41c3fcb7de2c4dcec33d42795e43a7da2
Jonathan Chen <2110511101010@live4.utcc.ac.th>
2023-03-16 11:42:45 +0700

    add code to display results on web

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/wordcnt', function(req, res) {
-  res.send("word count API");
+var fs = require('fs');
+function file_readline()
+{
+  let cnt = 0;
+  const contents  = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line => {
+    console.log(`Line from file: ${line}`);
+    if(line == fav_team)cnt++;
+  });
+  return cnt;
+}
+router.get('/wordcnt/fav_team', function(req, res) {
+  let fav_team = req.params.fav_team;
+  let cnt = file_readline(fav_team);
+  let str1 = "Your favorite team -> <b>" + fav_team + "</b><br>"
+  let str2 = "Number of occurrences -> <b>" + cnt + "</b><br>"
+  res.send("word count API" + fav_team + "("+cnt+")");
+  
 });
+
+
 module.exports = router;
