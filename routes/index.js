eaebe62dc821653db74d418305d731f2a2efa938
Natthapon <2110511101025@live4.utcc.ac.th>
2023-03-16 11:21:35 +0700

    add a readline function

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,19 @@ router.get('/', function(req, res, next) {
 router.get('/wordcnt', function(req, res) {
   res.send("word count API");
 });
+var fs = require('fs')
+function file_readline(){  
+  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
 
 router.get('/wordcnt/:fav_team', function(req, res) {
   let fav_team = req.params.fav_team;
+  file_readline();
   res.send("word count API" + fav_team);
 });
+
 
 module.exports = router;
