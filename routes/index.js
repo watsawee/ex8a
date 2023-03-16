bb2cba592dfad407572bfa657649005690a592f7
Rachata <2110511101041@live4.utcc.ac.th>
2023-03-16 11:21:00 +0700

    add a readline function

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+var fs = require('fs');
+function file_readline(){  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+    contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
 router.get('/wordcnt:fav_team', function(req, res) {
   let fav_team = req.params.fav_team;
+  file_readline();
   res.send("word count API - "+fav_team);
 });
 
