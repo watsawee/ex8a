4fd4aff7587657cbd10af1d0bb02538cbd6931ca
Watsawee <watsawee@gmail.com>
2023-03-16 11:42:50 +0700

    add code to display results on web

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,10 @@ function file_readline(fav_team){
 router.get('/wordcnt/:fav_team', function(req, res) {
   let fav_team = req.params.fav_team;
   let cnt = file_readline(fav_team);
-  res.send("word count API - "+fav_team+"("+cnt+")");
+  let heading = "<h2>Word Count Result</h2>";
+  let str1 = "Your favorite team -> <b>"+fav_team+"</b><br>";
+  let str2 = "Number of occurrences -> <b>"+cnt+"</b><br>";
+  res.send(heading+str1+str2);
 });
 
 module.exports = router;
